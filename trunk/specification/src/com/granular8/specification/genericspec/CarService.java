ca7666b4423ca083e74de3286804dcd4c6ed1fc7
patrikfr <patrikfr@a5383845-f73a-0410-a684-d5c8e9da82a5>
2007-09-25 21:46:36 +0000

    Removed auto generated doc

--- trunk/specification/src/com/granular8/specification/genericspec/CarService.java
+++ trunk/specification/src/com/granular8/specification/genericspec/CarService.java
@@ -7,13 +7,6 @@ import com.granular8.specification.domain.Car;
 import java.util.Set;
 import java.util.Collection;
 
-/**
- * Created by IntelliJ IDEA.
- * User: patrik
- * Date: Jan 31, 2007
- * Time: 8:06:10 AM
- * To change this template use File | Settings | File Templates.
- */
 public interface CarService {
   void setRepository(CarRepository repository);
 
