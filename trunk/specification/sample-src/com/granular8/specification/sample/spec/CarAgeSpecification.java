d56464170755b19018a2f50ef5244cc93ee3fa9e
patrikfr <patrikfr@a5383845-f73a-0410-a684-d5c8e9da82a5>
2007-10-19 20:53:28 +0000

    Fixed scenario test for sample spec. Implemented specs.

--- trunk/specification/sample-src/com/granular8/specification/sample/spec/CarAgeSpecification.java
+++ trunk/specification/sample-src/com/granular8/specification/sample/spec/CarAgeSpecification.java
@@ -1,9 +1,29 @@
 package com.granular8.specification.sample.spec;
 
 import com.granular8.specification.spec.AbstractSpecification;
+import com.granular8.specification.sample.domain.Car;
+import com.domainlanguage.time.CalendarDate;
+import com.domainlanguage.time.Duration;
+import com.domainlanguage.timeutil.Clock;
+
+import java.util.TimeZone;
 
 public class CarAgeSpecification extends AbstractSpecification {
+
+  final CalendarDate today = Clock.now().calendarDate(TimeZone.getTimeZone("GMT"));
+  final Duration maxAge;
+
+  public CarAgeSpecification(int maxYearsAge) {
+    maxAge = Duration.years(maxYearsAge);
+  }
+
   public boolean isSatisfiedBy(Object o) {
-    throw new UnsupportedOperationException();
+    if (o instanceof Car) {
+      Car car = (Car) o;
+      return maxAge.startingFrom(car.manufacturingDate()).includes(today);
+    } else {
+      throw new ClassCastException("I only deal with cars, you gave me: " +
+         o.getClass().getCanonicalName());
+    }
   }
 }
