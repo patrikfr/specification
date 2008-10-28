046f52a5132b8abd83f7de3eb48c3d42b6467687
Patrik Fredriksson <patrikfr@gmail.com>
2008-10-28 20:00:39 +0000

    Added more tests to verify new varags constructor.

--- specification/testsrc/com/granular8/specification/genericspec/OrSpecificationTest.java
+++ specification/testsrc/com/granular8/specification/genericspec/OrSpecificationTest.java
@@ -4,20 +4,35 @@ import junit.framework.TestCase;
 
 public class OrSpecificationTest extends TestCase {
 
-  public void testAndIsSatisifedBy() throws Exception {
+  public void testOrIsSatisifedBy() throws Exception {
     AlwaysTrueSpec trueSpec = new AlwaysTrueSpec();
     AlwaysFalseSpec falseSpec = new AlwaysFalseSpec();
 
     OrSpecification<Object> orSpecification = new OrSpecification<Object>(trueSpec, trueSpec);
     assertTrue(orSpecification.isSatisfiedBy(new Object()));
 
+    orSpecification = new OrSpecification<Object>(trueSpec, trueSpec, trueSpec);
+    assertTrue(orSpecification.isSatisfiedBy(new Object()));
+
     orSpecification = new OrSpecification<Object>(falseSpec, trueSpec);
+    assertTrue(orSpecification.isSatisfiedBy(new Object()));
+
+    orSpecification = new OrSpecification<Object>(falseSpec, trueSpec, trueSpec);
+    assertTrue(orSpecification.isSatisfiedBy(new Object()));
+
+    orSpecification = new OrSpecification<Object>(falseSpec, trueSpec, falseSpec);
+    assertTrue(orSpecification.isSatisfiedBy(new Object()));
+
+    orSpecification = new OrSpecification<Object>(falseSpec, falseSpec, trueSpec);
     assertTrue(orSpecification.isSatisfiedBy(new Object()));
 
     orSpecification = new OrSpecification<Object>(trueSpec, falseSpec);
     assertTrue(orSpecification.isSatisfiedBy(new Object()));
 
     orSpecification = new OrSpecification<Object>(falseSpec, falseSpec);
+    assertFalse(orSpecification.isSatisfiedBy(new Object()));
+
+    orSpecification = new OrSpecification<Object>(falseSpec, falseSpec, falseSpec);
     assertFalse(orSpecification.isSatisfiedBy(new Object()));
 
   }
